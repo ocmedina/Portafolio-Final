70623a7ab95ea03f3b8cd6031cebc0341857c442
ocmedina <octaviomedina239@gmail.com>
2025-07-24 16:10:49 -0300

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{astro,js,ts,jsx,tsx}'],
+export default {
+  darkMode: 'class', // 👈 muy importante
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+  ],
   theme: {
-    extend: {
-      keyframes: {
-        slide: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-50%)' },
-        },
-      },
-      animation: {
-        slide: 'slide 40s linear infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
