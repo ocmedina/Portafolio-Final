479ac6c13dd04e2b8a7ded46ac9536139f7dbda2
ocmedina <octaviomedina239@gmail.com>
2025-08-28 01:37:17 -0300

    feat: add dynamic role typing effect in Hero component and include Experiencias section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // 👈 muy importante
   content: [
     "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        typing: "typing 4s steps(30, end), blink-caret .75s step-end infinite",
+      },
+      keyframes: {
+        typing: {
+          from: { width: "0" },
+          to: { width: "100%" },
+        },
+        "blink-caret": {
+          "from, to": { "border-color": "transparent" },
+          "50%": { "border-color": "white" },
+        },
+      },
+    },
   },
   plugins: [],
 }
-
