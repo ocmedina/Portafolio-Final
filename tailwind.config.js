df6bfeee19acf9fa9c644b513cded9219fb0f2df
ocmedina <octaviomedina239@gmail.com>
2025-09-17 01:08:19 -0300

    fix section "proyecto"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+import lineClamp from '@tailwindcss/line-clamp';
+
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
-  ],
-  theme: {
-    extend: {
-      animation: {
-        typing: "typing 4s steps(30, end), blink-caret .75s step-end infinite",
-      },
-      keyframes: {
-        typing: {
-          from: { width: "0" },
-          to: { width: "100%" },
-        },
-        "blink-caret": {
-          "from, to": { "border-color": "transparent" },
-          "50%": { "border-color": "white" },
-        },
-      },
-    },
-  },
-  plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        typing: "typing 4s steps(30, end), blink-caret .75s step-end infinite",
+      },
+      keyframes: {
+        typing: {
+          from: { width: "0" },
+          to: { width: "100%" },
+        },
+        "blink-caret": {
+          "from, to": { "border-color": "transparent" },
+          "50%": { "border-color": "white" },
+        },
+      },
+    },
+  },
+  plugins: [
+    lineClamp,
+  ],
+};
